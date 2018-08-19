24f353b2cce35791f65f38d7977b37db03b4b9a8
James Battersby <carandja@gmail.com>
2018-08-19 12:42:58 +0100

    setup

--- flaskex/bin/activate.fish
+++ flaskex/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/data/data/com.termux/files/home/local/web/flaskex"
+set -gx VIRTUAL_ENV "/home/james/local/web/flaskex"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
